20333fcc0aeddb6b39109429095f6d0a9d7a38eb
CC <136565456@qq.com>
2021-10-20 15:48:33 +0800

    rps-css

--- rps.js
+++ rps.js
@@ -2,6 +2,8 @@ let playerScore = 0;
 let computerScore = 0;
 
 const buttons = document.querySelectorAll('button');
+const btn = document.querySelector('#btn');
+const input = document.querySelector('input');
 
 function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
@@ -17,15 +19,13 @@ function disableButtons() {
 function playRound(playerSelection) {
     let result = "";
     const computerSelection = computerPlay();
+    showChoice(playerSelection, computerSelection);
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'rock')) {
         playerScore++;
         result = ('You win! ' + playerSelection + ' beats ' + computerSelection + "<br>Player score: " + playerScore + '<br>Computer score: ' + computerScore);
-        if (playerScore == 5) {
-            result += '<br>You won the game! Reload the page to play again';
-            disableButtons()
-        }
+
     }
     else if (playerSelection == computerSelection) {
         result = ('It\'s a tie. You both chose ' + playerSelection + "<br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
@@ -34,20 +34,45 @@ function playRound(playerSelection) {
     else {
         computerScore++;
         result = ('You lose! ' + computerSelection + ' beats ' + playerSelection + '<br>Player score: ' + playerScore + '<br>Computer score: ' + computerScore);
-        if (computerScore == 5) {
-            result += '<br>I won the game! Reload the page to play again'
-            disableButtons()
-        }
     }
+    gameOver(playerScore, computerScore);
     document.getElementById('result').innerHTML = result
 
 }
 
 buttons.forEach(button => {
     button.addEventListener('click', function () {
-        playRound(button.value)
+        playRound(button.value);
     });
 });
+
+const $playerSelection = document.querySelector('#human-selection');
+const $computerSelection = document.querySelector('#computer-selection')
+
+function showChoice(playerSelection, computerSelection) {
+    $playerSelection.className = '';
+    $computerSelection.className = '';
+    $playerSelection.classList.add(playerSelection);
+    $computerSelection.classList.add(computerSelection);
+}
+
+function reloadPage() {
+    window.location.reload()
+}
+
+function gameOver(playerScore, computerScore) {
+    if (playerScore == 5 || computerScore == 5) {
+        disableButtons()
+        btn.classList.toggle('btn');
+        input.classList.toggle('hidden');
+        if (playerScore == 5) {
+            result += '<br>You won the game! Reload the page to play again';
+        }
+        else {
+            result += '<br>I won the game! Reload the page to play again';
+        }
+    }
+}
 
 
 
